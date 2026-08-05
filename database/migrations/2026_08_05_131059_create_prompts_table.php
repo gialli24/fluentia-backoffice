d04fc06e8672ce11c8b943301fcf0fafc8dd1bfd
gialli24 <a.gialli2409@gmail.com>
2026-08-05 15:16:27 +0200

    add migrations

---
+++ database/migrations/2026_08_05_131059_create_prompts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prompts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->text('content');
+            $table->text('instructions')->nullable();
+            $table->enum('output_type', array('text', 'json', 'html', 'image'))->default('text');
+            $table->text('thumbnail')->nullable();
+            $table->integer('copy_count')->default(0);
+            $table->tinyInteger('is_featured')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prompts');
+    }
+};
